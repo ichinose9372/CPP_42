753143ce86459ca18713fc6687fccf9c6ad07b05
Yuki Ichinose <yichinos@c1r16s4.42tokyo.jp>
2023-08-24 16:31:29 +0900

    +

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yichinos <yichinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 16:21:06 by yichinos          #+#    #+#             */
+/*   Updated: 2023/08/24 16:22:40 by yichinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(std::string str);
+		~ScalarConverter();
+};
+
+#endif
---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yichinos <yichinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 16:20:09 by yichinos          #+#    #+#             */
+/*   Updated: 2023/08/24 16:23:38 by yichinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: wrong number of arguments" << std::endl;
+		return (1);
+	}
+	ScalarConverter sc(argv[1]);
+	return (0);
+}
