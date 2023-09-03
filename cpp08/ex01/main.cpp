56bef38ea923e321767f99a18c35d6bf10c4515a
yichinos <ichinose.0720.emt@gmail.com>
2023-09-03 16:50:17 +0900

    +

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ichinoseyuuki <ichinoseyuuki@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:59:00 by yichinos          #+#    #+#             */
-/*   Updated: 2023/09/03 12:32:27 by ichinoseyuu      ###   ########.fr       */
+/*   Updated: 2023/09/03 16:49:10 by ichinoseyuu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,6 @@ int main(void)
 		{
 			std::cerr << e.what() << std::endl;
 		}
-		
-		
 	}
 	{
 		std::cout << "----------corect array --------------------\n";
@@ -84,7 +82,24 @@ int main(void)
 		{
 			std::cerr << e.what() << std::endl;
 		}
-		
+	}
+	{
+		std::cout << "------over 10k numbers---------------------\n";
+		Span sp = Span(12000);
+	    srand(time(NULL));
+		try
+		{
+			for (size_t i = 0; i < 12000; i++)
+			{
+				sp.addNumber(rand());
+			}
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;	
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr<< e.what() << std::endl;
+		}
 		
 	}
 	return (0);
