6aa2c875d6aa8974e7fa608e39cf163d6a94a0f7
Yuki Ichinose <yichinos@c1r23s10.42tokyo.jp>
2023-09-13 17:33:23 +0900

    push:

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -21,42 +21,42 @@ void test_scalaeconvert(char *str)
 	std::cout << "-----------------------------------------------------------" << std::endl;
 }
 
-// int main(int argc, char **argv)
-// {
-// 	if (argc != 2)
-// 	{
-// 		std::cerr << "Error: wrong number of arguments" << std::endl;
-// 		return (1);
-// 	}
-	
-// 	ScalarConverter::convert(argv[1]);
-	
-// 	return (0);
-// }
-
-int main(void)
+int main(int argc, char **argv)
 {
-	char str0[] = "nan";
-	test_scalaeconvert(str0);
-	char str1[] = "42.0f";
-	test_scalaeconvert(str1);
-	char str2[] = "0";
-	test_scalaeconvert(str2);
-	char str3[] = "2147483647";
-	test_scalaeconvert(str3);
-	char str4[] = "2147483648";
-	test_scalaeconvert(str4);
-	char str5[] = "4.4444444444444444444";
-	test_scalaeconvert(str5);
-	char str6[] = "4.44444f";
-	test_scalaeconvert(str6);
-	char str7[] = "0.00000000000001";
-	test_scalaeconvert(str7);
-	char str8[] = "0000000000000000.0000001";
-	test_scalaeconvert(str8);
-	char str9[] = "nanf";
-	test_scalaeconvert(str9);
-	char str10[] = "10000000000000000000000000000000000000000";
-	test_scalaeconvert(str10);
-
+	if (argc != 2)
+	{
+		std::cerr << "Error: wrong number of arguments" << std::endl;
+		return (1);
+	}
+	
+	ScalarConverter::convert(argv[1]);
+	
+	return (0);
 }
+
+// int main(void)
+// {
+// 	char str0[] = "nan";
+// 	test_scalaeconvert(str0);
+// 	char str1[] = "42.0f";
+// 	test_scalaeconvert(str1);
+// 	char str2[] = "0";
+// 	test_scalaeconvert(str2);
+// 	char str3[] = "2147483647";
+// 	test_scalaeconvert(str3);
+// 	char str4[] = "2147483648";
+// 	test_scalaeconvert(str4);
+// 	char str5[] = "4.4444444444444444444";
+// 	test_scalaeconvert(str5);
+// 	char str6[] = "4.44444f";
+// 	test_scalaeconvert(str6);
+// 	char str7[] = "0.00000000000001";
+// 	test_scalaeconvert(str7);
+// 	char str8[] = "0000000000000000.0000001";
+// 	test_scalaeconvert(str8);
+// 	char str9[] = "nanf";
+// 	test_scalaeconvert(str9);
+// 	char str10[] = "10000000000000000000000000000000000000000";
+// 	test_scalaeconvert(str10);
+
+// }
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yichinos <yichinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 07:31:27 by yichinos          #+#    #+#             */
-/*   Updated: 2023/09/13 13:36:01 by yichinos         ###   ########.fr       */
+/*   Updated: 2023/09/13 17:29:11 by yichinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,70 @@
 #include "C.hpp"
 #include <cstdlib>
 
+Base* generate(void)
+{
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	int randVal = std::rand();
+
+	if (randVal % 3 == 0)
+		return new A;
+	else if (randVal % 3 == 1)
+		return new B;
+	else
+		return new C;
+}
+
+void test_generate_ptr(Base *base)
+{
+	if (dynamic_cast<A*>(base))
+		std::cout << GREEN << " is (  A  )" << NORMAL << std::endl;
+	else if (dynamic_cast<B*>(base))
+		std::cout << GREEN << " is (  B  )" << NORMAL << std::endl;
+	else if (dynamic_cast<C*>(base))
+		std::cout << GREEN << " is (  C  )" << NORMAL << std::endl;
+	else 
+		std::cout << RED << "Unknown type!" << NORMAL << std::endl;
+}
+
+void test_generate_ref(Base &base)
+{	
+	try
+	{
+		A& a = dynamic_cast<A&>(base);
+		(void) a;
+		std::cout << GREEN << " is (  A  )" << NORMAL << std::endl;
+		return ;
+	}
+	catch (const std::bad_cast&) {}
+	try
+	{
+		B& b = dynamic_cast<B&>(base);
+		(void) b;
+		std::cout << GREEN  << " is (  B  )" << NORMAL << std::endl;
+		return ;
+	}
+	catch (const std::bad_cast&) {}
+	try
+	{
+		C& c= dynamic_cast<C&>(base);
+		(void) c;
+		std::cout << GREEN  << " is (  C  )" << NORMAL << std::endl;
+		return ;
+	}
+	catch (const std::bad_cast&) {}
+	std::cout << RED << "Unknown type!" << NORMAL << std::endl;
+	
+}
+
 int main(void)
 {
+	Base *base = generate();
+
+	std::cout << "Base pointer = "  << base << std::endl;
+	test_generate_ptr(base);
+	
+	std::cout << "Base reference = " << &base << std::endl;
+	test_generate_ref(*base);
 	
 	
 	return (0);
